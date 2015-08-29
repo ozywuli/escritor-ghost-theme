06555af07150f9f540032042b27ddf388880ad84
ozywuli <ozy.wu.li@gmail.com>
2015-08-29 16:55:40 -0700

    recent articles

--- dev/js/webkid.js
+++ dev/js/webkid.js
@@ -1,56 +1,3 @@
-(function() {
-
-var $menuToggle;
-var menuOpened;
-var $menu = $('.nav');
-
-$menuToggle = $('.menu-toggle');
-menuOpened = 'nav--opened';
-
-
-var toggleMenu = {
-  init: function() {
-    console.log(this);
-    $menuToggle.click( toggleMenu.toggleMenu );
-  },
-  toggleMenu: function() {
-
-    if ($menu.hasClass(menuOpened)) {
-      $menu.removeClass(menuOpened);
-    } else {
-      $menu.addClass(menuOpened);
-    }
-
-  }
-}
-
-
-var bodyClick = {
-  init: function() {
-    $('body').click( 
-      bodyClick.closeMenu 
-    );
-  },
-  closeMenu: function(e) {
-    console.log(e.target);
-
-    if ( !$menu.is(e.target) && !$menuToggle.find('*').is(e.target) ) {
-      if ($menu.hasClass(menuOpened)) {
-        console.log(true);
-        $menu.removeClass(menuOpened);
-      }
-    }
-
-  }
-}
-
-$(document).ready(function() {
-  toggleMenu.init();
-  bodyClick.init();
-});
-
-
-
 $(function() {
 
   var months = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'NOV', 'DEC'];
@@ -87,8 +34,3 @@ $(function() {
   }).success(renderSite);
 
 });
-
-
-
-
-})(jQuery);
